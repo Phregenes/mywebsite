f5ee417f08c1ca1a88feb3ecc7332c8ae41ebe77
Pedro Regenes <phregenes@gmail.com>
2020-02-27 13:45:07 -0300

    Feat"bar"

--- src/components/Bar/index.js
+++ src/components/Bar/index.js
@@ -1,45 +1,29 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Carousel } from 'antd';
+import Header from 'antd/lib/calendar/Header';
+import github from '../../assets/img/github.png';
+import linkedin from '../../assets/img/linkedin.png';
 
-import './styles.css';
 
-import linkedin from '../../linkedin.png';
-import github from '../../github.png';
- 
-function Bar(){
-  return(
-    <main>
-        <ul>
-        <li className="dev-item">
-            <header>
-              <img src={github} alt="Pedro Regenes" />
-              <div className="user-info">
-                <strong>GitHub</strong>
-                <a href="https://github.com/Phregenes">Acessar perfil no github</a>
-              </div>
-            </header>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src={linkedin} alt="Pedro Regenes" />
-              <div className="user-info">
-                <strong>Linkedin</strong>
-                <a href="https://www.linkedin.com/in/phregenes/">Acessar perfil no Linkedin</a>
-              </div>
-            </header>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src={linkedin} alt="Pedro Regenes" />
-              <div className="user-info">
-                <strong>Linkedin</strong>
-                <a href="https://www.linkedin.com/in/phregenes/">Acessar perfil no Linkedin</a>
-              </div>
-            </header>
-          </li>
-          </ul>
-          </main>
-  );
-};
-
+const Bar = () =>(
+  <Carousel autoplay>
+    <div>
+      <h3>ola mundo</h3>
+    </div>
+    <div className="user-info">
+      <img src={github} alt="Pedro Regenes" />
+      <strong>GitHub</strong>
+      <a href="https://github.com/Phregenes">Acessar perfil no github</a>
+    </div>
+    <div className="user-info">
+      <img src={linkedin} alt="Pedro Regenes" />
+      <strong>Linkedin</strong>
+      <a href="https://www.linkedin.com/in/phregenes/">Acessar perfil no Linkedin</a>
+    </div>
+  </Carousel>
+);
   
-  export default Bar;
+export default Bar;
